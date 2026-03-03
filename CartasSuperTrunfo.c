ed65d4f07b53195455b439aa0e04e3c54637ec3c
Mateus Arruda <teteuarrudap@gmail.com>
2026-03-03 18:19:08 -0300

    Defini as variáveis das duas cidades
    Added variables for two city cards including population, tourist points, state, code, name, GDP, and area.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,35 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  
+  // Carta 01
+  int populacao01;
+  int pontos_turisticos01;
+  char estado01;
+  char codigocarta01[50];
+  char nome_cidade01[50];
+  float pib01;
+  float area01;
+
+  // Carta 02
+  int populacao02;
+  int pontos_turisticos02;
+  char estado02;
+  char codigocarta02[50];
+  char nome_cidade02[50]
+  float pib02;
+  float area02;
+  
   // Área para entrada de dados
 
+  // Carta 01
+  printf("Qual o nome da sua cidade? \n");
+  scanf("%s", &nome_cidade01);
+
+
+
+
+  
   // Área para exibição dos dados da cidade
 
 return 0;
